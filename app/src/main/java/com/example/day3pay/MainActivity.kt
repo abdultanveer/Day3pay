3b98721fc7679715c4b0bb18f711fc80a0bf30d7
abdul <abdul.tanveer@gmail.com>
2025-10-16 13:55:37 +0530

    handle button clicks

--- app/src/main/java/com/example/day3pay/MainActivity.kt
+++ app/src/main/java/com/example/day3pay/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.day3pay
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainTv:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainTv = findViewById(R.id.tvMain)
 
+    }
+
+    fun handleClick(view: View) {
+        mainTv.setText("hello paypal -- android")
     }
 }
