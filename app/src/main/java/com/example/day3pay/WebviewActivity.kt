443e33e46ceb08ef8a261500bcdddbec7de154ce
abdul <abdul.tanveer@gmail.com>
2025-10-22 17:17:12 +0530

    intercept loading of success.html and instead load MainActivity

--- app/src/main/java/com/example/day3pay/WebviewActivity.kt
+++ app/src/main/java/com/example/day3pay/WebviewActivity.kt
@@ -1,8 +1,10 @@
 package com.example.day3pay
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Base64
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
@@ -17,7 +19,7 @@ class WebviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
         webview = findViewById(R.id.webView)
-       // webview.loadUrl("https://yahoo.com")
+        // webview.loadUrl("https://yahoo.com")
         webview.settings.javaScriptEnabled = true
         webview.addJavascriptInterface(WebAppInterface(this), "Android")
 
@@ -31,11 +33,29 @@ class WebviewActivity : AppCompatActivity() {
         webview.loadUrl("file:///android_asset/myscript.html")
 
 
-//        val unencodedHtml =
+        webview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                request: WebResourceRequest
+            ): Boolean {
+                val url = request.url.toString()
+
+                // Intercept if it's trying to load success.html
+                if (url.contains("success.html")) {
+                    // Redirect to native Android activity
+                    startActivity(Intent(this@WebviewActivity, MainActivity::class.java))
+                    return true // Prevent WebView from loading it
+                }
+
+                return false // Allow other URLs to load
+            }
+
+
+            //        val unencodedHtml =
 //            "<html><body>'%23' is the percent code for ‘#‘ </body></html>";
 //        val encodedHtml = Base64.encodeToString(unencodedHtml.toByteArray(), Base64.NO_PADDING)
 //        webview.loadData(encodedHtml, "text/html", "base64")
-        val html = """
+            val html = """
             <html>
             <body>
                 <h2>JS Demo</h2>
@@ -49,7 +69,8 @@ class WebviewActivity : AppCompatActivity() {
 //        webview.evaluateJavascript("greetUser('Kotlin')") { result ->
 //            println("JS returned: $result")
 //        }
-       // webview.loadDataWithBaseURL(null, html, "text/html", "utf-8", null)
+            // webview.loadDataWithBaseURL(null, html, "text/html", "utf-8", null)
 
+        }
     }
 }
